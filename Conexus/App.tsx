1a8dccf845f24a7df12b5bb38d44ec00fc9cb74d
dilshan.d3 <dilshan72404@gmail.com>
2025-03-22 13:25:28 +0530

    testing some feature

---
+++ Conexus/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>hero i am </Text>
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({})
